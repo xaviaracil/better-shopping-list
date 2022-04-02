12f966432d7f8413f18aad46e211c65259788593
Xavi Aracil <xaracil@uoc.edu>
2022-04-02 21:47:44 +0200

    Added test when current list exists

--- BetterShoppingListTests/CurrentListTest.swift
+++ BetterShoppingListTests/CurrentListTest.swift
@@ -22,8 +22,35 @@ final class CurrentListTest: XCTestCase {
         // when asking for products of the current list
         let products = try context.fetch(CurrentListQueries.productsFetchRequest)
 
-        // then wh get an empty list
+        // then we get an empty list
         XCTAssertNotNil(products)
         XCTAssertTrue(products.isEmpty)
+    }
+
+    func testGivenCurrentListFetchReturnItsProducts() throws {
+        // given a current list
+        let list = ShoppingList(context: context)
+        list.timestamp = Date()
+        list.name = "Current List"
+        list.isCurrent = true
+
+        let product = ChosenProduct(context: context)
+        product.name = "Product 1"
+        product.price = 1.50
+
+        list.addToProducts(product)
+
+        try context.save()
+
+        // when asking for products of the current list
+        let products = try context.fetch(CurrentListQueries.productsFetchRequest)
+
+        // then we get a list
+        XCTAssertNotNil(products)
+        XCTAssertFalse(products.isEmpty)
+        XCTAssertEqual(1, products.count)
+        XCTAssertEqual(product.price, products.first?.price)
+        XCTAssertEqual(product.name, products.first?.name)
+        XCTAssertEqual(list.name, products.first?.list?.name)
     }
 }
