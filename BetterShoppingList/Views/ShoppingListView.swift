b5daefb339299ab31349d4aac1557e8de10198ac
Xavi Aracil <xaracil@uoc.edu>
2022-04-02 20:21:17 +0200

    Added view for displaying a shopping list

---
+++ BetterShoppingList/Views/ShoppingListView.swift
@@ -0,0 +1,49 @@
+//
+//  ShoppingListView.swift
+//  BetterShoppingList
+//
+//  Created by Xavi Aracil on 2/4/22.
+//
+
+import SwiftUI
+
+struct ShoppingListView: View {
+    var list: ShoppingList
+
+    var body: some View {
+
+        Group {
+            Label(list.name ?? "No Name", systemImage: "cart")
+                .labelStyle(ShoppingListLabelStyle())
+                .padding(EdgeInsets(top: 30.0, leading: 20.0, bottom: 10.0, trailing: 20.0))
+        }
+        .addBorder(.foreground, width: 1, cornerRadius: 10)
+    }
+}
+
+struct ShoppingListLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        VStack {
+            configuration.icon
+                .font(.system(size: 96))
+                .foregroundColor(.accentColor)
+            configuration.title
+                .font(.headline)
+        }
+    }
+}
+struct ShoppingListView_Previews: PreviewProvider {
+    static var previews: some View {
+        let query = ShoppingListQueries(context: PersistenceController.preview.container.viewContext)
+        let list = try? query.savedLists().first
+        ShoppingListView(list: list!)
+    }
+}
+
+extension View {
+    public func addBorder<S>(_ content: S, width: CGFloat = 1, cornerRadius: CGFloat) -> some View where S: ShapeStyle {
+        let roundedRect = RoundedRectangle(cornerRadius: cornerRadius)
+        return clipShape(roundedRect)
+             .overlay(roundedRect.strokeBorder(content, lineWidth: width))
+    }
+}
