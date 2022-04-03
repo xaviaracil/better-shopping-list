1db93cfffd5f08096f1562de5d3e68e51ecdbfb3
Xavi Aracil <xaracil@uoc.edu>
2022-04-03 15:17:52 +0200

    Added query method, returning a FetchRequest

--- BetterShoppingList/Persistence/Queries.swift
+++ BetterShoppingList/Persistence/Queries.swift
@@ -11,6 +11,34 @@ import CoreData
 struct OfferQueries {
     let context: NSManagedObjectContext
 
+    static func queryFetchRequest(text: String, markets: [String] = []) -> NSFetchRequest<Offer> {
+        let fetchRequest = Offer.fetchRequest()
+        fetchRequest.predicate = predicateFor(text: text, markets: markets)
+        fetchRequest.relationshipKeyPathsForPrefetching = ["market", "product"]
+        fetchRequest.sortDescriptors = [
+            NSSortDescriptor(keyPath: \Offer.product?.name, ascending: true), // by product
+            NSSortDescriptor(keyPath: \Offer.price, ascending: true) // then by price
+        ]
+        return fetchRequest
+    }
+
+    private static func predicateFor(text: String, markets: [String]) -> NSPredicate {
+        let words = text.components(separatedBy: " ")
+        var textPredicates: [NSPredicate] = []
+        for word in words {
+            let predicate = NSPredicate(format: "product.name CONTAINS[cd] %@", word)
+            textPredicates.append(predicate)
+        }
+        let namePredicate = NSCompoundPredicate(andPredicateWithSubpredicates: textPredicates)
+
+        if !markets.isEmpty {
+            let marketsPredicate = NSPredicate(format: "market.name IN %@", markets)
+            return NSCompoundPredicate(andPredicateWithSubpredicates: [namePredicate, marketsPredicate])
+        }
+
+        return namePredicate
+    }
+
     func query(text: String, markets: [String] = []) throws -> [Offer] {
         let fetchRequest = Offer.fetchRequest()
         fetchRequest.predicate = predicateFor(text: text, markets: markets)
