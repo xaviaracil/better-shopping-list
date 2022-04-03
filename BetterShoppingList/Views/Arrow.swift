f483f5ef352330e854f0499df7a2b45de0f4e032
Xavi Aracil <xaracil@uoc.edu>
2022-04-03 19:38:53 +0200

    New Arrow implementation, matching design

--- BetterShoppingList/Views/Arrow.swift
+++ BetterShoppingList/Views/Arrow.swift
@@ -7,44 +7,68 @@
 
 import SwiftUI
 
-struct Arrow: Shape {
-    struct Contants {
+struct Arrow: View {
+    let lineWidth: CGFloat
+
+    init(lineWidth: CGFloat = 10.0) {
+        self.lineWidth = lineWidth
+    }
+
+    struct Constants {
         // swiftlint:disable nesting type_name
         struct X {
-            static let center = 0.5
-            static let left = 0.49
-            static let right = 0.51
-            static let leftVertex = 0.45
-            static let rightVertex = 0.55
+            static let controlPoint = 60.0
+            static let arrowLengthLeft = 40.0
+            static let arrowLengthRight = 30.0
+            static let vertexOffsetLeft = 3.0
+            static let vertexOffsetRight = 1.0
 
         }
         // swiftlint:disable nesting type_name
         struct Y {
-            static let top = 1.0
-            static let topArrow = 0.95
-            static let bottomArrow = 0.15
-            static let bottom = 0.1
+            static let controlPoint = 10.0
+            static let arrowLengthLeft = 20.0
+            static let arrowLengthRight = 30.0
+            static let vertexOffsetLeft = 1.0
+            static let vertexOffsetRight = 1.0
         }
-
     }
-    func path(in rect: CGRect) -> Path {
-        Path { path in
-            let width = rect.width
-            let height = rect.height
 
-            path.addLines( [
-                CGPoint(x: width * Contants.X.center, y: height * Contants.Y.top),
-                CGPoint(x: width * Contants.X.leftVertex, y: height * Contants.Y.topArrow),
-                CGPoint(x: width * Contants.X.left, y: height * Contants.Y.topArrow),
-                CGPoint(x: width * Contants.X.left, y: height * Contants.Y.bottomArrow),
-                CGPoint(x: width * Contants.X.leftVertex, y: height * Contants.Y.bottomArrow),
-                CGPoint(x: width * Contants.X.center, y: height * Contants.Y.bottom),
-                CGPoint(x: width * Contants.X.rightVertex, y: height * Contants.Y.bottomArrow),
-                CGPoint(x: width * Contants.X.right, y: height * Contants.Y.bottomArrow),
-                CGPoint(x: width * Contants.X.right, y: height * Contants.Y.topArrow),
-                CGPoint(x: width * Contants.X.rightVertex, y: height * Contants.Y.topArrow)
-            ])
-            path.closeSubpath()
+    var body: some View {
+        GeometryReader { reader in
+            let midX = reader.size.width / 2
+            let midY = reader.size.height / 2
+            let maxY = reader.size.height
+            let minY = 0.0
+
+            let bottomPoint = CGPoint(x: midX, y: maxY)
+            let topPoint = CGPoint(x: midX, y: minY)
+            let midPoint = CGPoint(x: midX, y: midY)
+
+            ZStack {
+                Path { path in
+                    path.move(to: bottomPoint)
+                    // swiftlint:disable line_length
+                    path.addCurve(to: topPoint,
+                                  control1: midPoint.delta(dx: -Constants.X.controlPoint, dy: -Constants.Y.controlPoint),
+                                  control2: midPoint.delta(dx: -Constants.X.controlPoint, dy: Constants.Y.controlPoint))
+                }.stroke(lineWidth: self.lineWidth)
+
+                Path { path in
+                    path.move(to: topPoint.delta(dx: Constants.X.vertexOffsetLeft, dy: -Constants.Y.vertexOffsetLeft))
+                    path.addLine(to: topPoint.delta(dx: -Constants.X.arrowLengthLeft, dy: Constants.Y.arrowLengthLeft))
+                    // swiftlint:disable line_length
+                    path.move(to: topPoint.delta(dx: -Constants.X.vertexOffsetRight, dy: -Constants.Y.vertexOffsetRight))
+                    path.addLine(to: topPoint.delta(dx: Constants.X.arrowLengthRight, dy: Constants.Y.arrowLengthRight))
+                }.stroke(lineWidth: self.lineWidth)
+            }
         }
+    }
+}
+
+extension CGPoint {
+    // swiftlint:disable identifier_name
+    func delta(dx: CGFloat, dy: CGFloat) -> CGPoint {
+        return CGPoint(x: self.x + dx, y: self.y + dy)
     }
 }
