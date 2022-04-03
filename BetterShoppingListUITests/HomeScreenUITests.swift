369f37efa2a0dc7e103e463f7128f2f9b667c74c
Xavi Aracil <xaracil@uoc.edu>
2022-04-03 15:17:22 +0200

    Renamed and refactored

--- BetterShoppingListUITests/HomeScreenUITests.swift
+++ BetterShoppingListUITests/HomeScreenUITests.swift
@@ -7,7 +7,9 @@
 
 import XCTest
 
-class BetterShoppingListUITests: XCTestCase {
+class HomeScreenUITests: XCTestCase {
+
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,6 +19,13 @@ class BetterShoppingListUITests: XCTestCase {
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your
         // tests before they run. The setUp method is a good place to do this.
+
+        // UI tests must launch the application that they test.
+        app = XCUIApplication()
+        app.launch()
+
+        // wait for the splash screen
+        sleep(4)
     }
 
     override func tearDownWithError() throws {
@@ -24,13 +33,6 @@ class BetterShoppingListUITests: XCTestCase {
     }
 
     func testDisplaySearchBar() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // wait for the splash screen
-        sleep(4)
-
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         let searchField = app.searchFields.element
@@ -39,13 +41,6 @@ class BetterShoppingListUITests: XCTestCase {
     }
 
     func testDisplayInitialText() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // wait for the splash screen
-        sleep(4)
-
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssertTrue(app.staticTexts["Start searching products"].exists)
