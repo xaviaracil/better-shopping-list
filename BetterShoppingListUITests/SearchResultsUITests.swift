e999fb225894aa46a95914de76265bbed7eafb47
Xavi Aracil <xaracil@uoc.edu>
2022-04-07 21:33:43 +0200

    removed default comment

--- BetterShoppingListUITests/SearchResultsUITests.swift
+++ BetterShoppingListUITests/SearchResultsUITests.swift
@@ -17,9 +17,6 @@ class SearchResultsUITest: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your
-        // tests before they run. The setUp method is a good place to do this.
-
         // UI tests must launch the application that they test.
         app = XCUIApplication()
         app.launch()
@@ -77,9 +74,6 @@ class SearchResultsUITest: XCTestCase {
         XCTAssertFalse(estrellaDamm.exists)
         let moritz = app.staticTexts["Cervesa Moritz 33"]
         XCTAssertFalse(moritz.exists)
-        // And market of the product must be visible
-        let market2 = app.staticTexts["Market 2"]
-        XCTAssertTrue(market2.waitForExistence(timeout: 5))
 
     }
 }
