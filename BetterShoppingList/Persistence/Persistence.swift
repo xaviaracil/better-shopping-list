8e3debe8121e1b295f97560fd6bc1e5a6dd55f53
Xavi Aracil <xaracil@uoc.edu>
2022-05-02 20:18:15 +0200

    Different images for each test market and products

--- BetterShoppingList/Persistence/Persistence.swift
+++ BetterShoppingList/Persistence/Persistence.swift
@@ -20,22 +20,27 @@ struct PersistenceController {
             try viewContext.deleteAllObjects()
 
             var markets: [Market] = []
-            for name in ["Sorli", "Carrefour", "BonPreu Esclat"] {
+            // swiftlint:disable line_length
+
+            for (name, icon) in ["Sorli": "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-19/140360878_111790920848567_5837078838760084208_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=111&_nc_ohc=p1nyNQap-coAX_wk-IJ&edm=ALbqBD0BAAAA&ccb=7-4&oh=00_AT8kF6h9NznYrD6pq17X9iHSmd0yvBnDWA6u1kGJ5xWSJA&oe=62599002&_nc_sid=9a90d6",
+                         "Carrefour": "https://pbs.twimg.com/profile_images/1501460589612306438/mzRWN7ev_400x400.jpg",
+                         "BonPreu Esclat": "https://pbs.twimg.com/profile_images/1103993935419068416/f8FkyYcp_400x400.png"] {
                 let market = Market(context: viewContext)
                 market.name = name
                 market.uuid = UUID()
-                // swiftlint:disable line_length
-                market.iconUrl = URL(string: "https://pbs.twimg.com/profile_images/1103993935419068416/f8FkyYcp_400x400.png")
+                market.iconUrl = URL(string: icon)
                 markets.append(market)
             }
 
-            let names = ["Cervesa Estrella Damm", "Cervesa Moritz 33", "Llet ATO 1L"]
+            // swiftlint:disable line_length
+            let products = ["Cervesa Estrella Damm": "https://www.compraonline.bonpreuesclat.cat/images-v3/dcbcfd72-cf23-44a2-8e14-8a38edd645a3/4d4e1ec4-18ec-4982-8267-90b2a6ec90db/300x300.jpg",
+                         "Cervesa Moritz 33": "https://www.compraonline.bonpreuesclat.cat/images-v3/dcbcfd72-cf23-44a2-8e14-8a38edd645a3/f39437cc-7536-4355-afd4-4cd266a6ea3e/300x300.jpg",
+                         "Llet ATO 1L": "https://static.condisline.com/resize_395x416/images/catalog/large/704005.jpg"]
             var offers: [String: [Offer]] = [:]
-            for name in names {
+            for (name, image) in products {
                 let product = Product(context: viewContext)
                 product.name = name
-                // swiftlint:disable line_length
-                product.imageUrl = URL(string: "https://static.condisline.com/resize_395x416/images/catalog/large/704005.jpg")
+                product.imageUrl = URL(string: image)
 
                 var productOffers: [Offer] = []
                 // load some offers
@@ -60,7 +65,7 @@ struct PersistenceController {
                 newList.name = "List \(index + 1)"
                 newList.isFavorite = Bool.random()
                 newList.earning = Double.random(in: 0...(16.0))
-                for name in names {
+                for (name, _) in products {
                     let chosenProduct = ChosenProduct(context: viewContext)
                     let offer = offers[name]?.randomElement()
                     chosenProduct.quantity = Int16.random(in: 1..<10)
